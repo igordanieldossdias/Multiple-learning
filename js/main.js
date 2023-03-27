6abc4a4442aa1f1075f049069e983243f2ac1b54
pedroolucaszzpl <125031536+pedroolucaszzpl@users.noreply.github.com>
2023-03-27 15:45:49 -0300

    formato

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function revisar(){
-    alert("redirecionando para o quiz");
+    alert("Redirecionando para o quiz...");
     window.location.href = "https://wordwall.net/pt/embed/7060c422887d46d6b5f0257315f4579a?themeId=1&templateId=5&fontStackId=0%22"
       
 }
